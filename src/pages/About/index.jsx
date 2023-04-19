fef545fe5481bf47d64915a2da82c04ad8ebb7e7
abnerfirmino <abnercasteliano@outlook.com>
2023-04-18 21:14:03 -0300

    Link para usuário autenticado

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 const About = () => {
 
   return (
-    <div>
-      <h1>About</h1>
+    <div className='about'>
+      <h1>Sobre o Mini <span>Blog</span></h1>
+      <p>Este projeto é um blog feito com React no front-end e Firebase no back-end.</p>
+      <Link to="/posts/create" className='btn'>Novo post</Link>
     </div>
   );
 }
