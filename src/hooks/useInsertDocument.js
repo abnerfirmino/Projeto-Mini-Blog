7d75638c9ecc94561bb9f25250c275ec1805af33
abnerfirmino <abnercasteliano@outlook.com>
2023-04-26 21:50:46 -0300

    custom hook use insert document ajustado

--- src/hooks/useInsertDocument.js
+++ src/hooks/useInsertDocument.js
@@ -3,43 +3,30 @@ import { useEffect, useReducer, useState } from 'react';
 import { collection, addDoc, Timestamp } from 'firebase/firestore';
 
 const initialState = {
-  loading: null,
+  loading: false,
   error: null,
 };
 
 const insertReducer = (state, action) => {
   switch (action.type) {
     case 'LOADING':
-      return { loading: true, error: null };
+      return { ...state, loading: true, error: null };
     case 'INSERTED_DOC':
-      return { loading: false, error: null };
+      return { ...state, loading: false, error: null };
     case 'ERROR':
-      return { loading: false, error: action.payload };
+      return { ...state, loading: false, error: action.payload };
     default:
-      return state;
+      return { ...state };
   }
 };
 
 const useInsertDocument = (docCollection) => {
   const [response, dispatch] = useReducer(insertReducer, initialState);
 
-  // deal with memory leak
-  const [cancelled, setCancelled] = useState(false);
-
-  const checkCancelledBeforeDispatch = (action) => {
-    if (!cancelled) {
-      dispatch(action);
-    } else {
-      return false;
-    }
-  };
-
   const insertDocument = async (document) => {
-    checkCancelledBeforeDispatch({
-      type: 'LOADING',
-    });
-
     try {
+      dispatch({ type: 'LOADING' });
+
       const newDocument = { ...document, createdAt: Timestamp.now() };
 
       const insertedDocument = await addDoc(
@@ -47,21 +34,17 @@ const useInsertDocument = (docCollection) => {
         newDocument,
       );
 
-      checkCancelledBeforeDispatch({
+      dispatch({
         type: 'INSERTED_DOC',
         payload: insertedDocument,
       });
     } catch (error) {
-      checkCancelledBeforeDispatch({
+      dispatch({
         type: 'ERROR',
         payload: error.message,
       });
     }
   };
-
-  useEffect(() => {
-    return () => setCancelled(true);
-  }, []);
 
   return { insertDocument, response };
 };
