41021fafa3636c71bea9401b6190b59fb7658191
abnerfirmino <abnercasteliano@outlook.com>
2023-05-03 12:00:37 -0300

    hook use Fetch criado

---
+++ src/hooks/useFetchDocuments.js
@@ -0,0 +1,66 @@
+import { useState, useEffect, useRef } from 'react';
+import { db } from '../firebase/config';
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  where,
+} from 'firebase/firestore';
+
+const useFetchDocuments = (docCollection, search = null, uid = null) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  // tratando memory leak
+  const isCancelled = useRef(false);
+
+  function checkIfIsCancelled() {
+    if (isCancelled.current) {
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    const loadData = async () => {
+      checkIfIsCancelled();
+
+      const collectionRef = await collection(db, docCollection);
+
+      try {
+        setLoading(true);
+
+        let q;
+
+        q = await query(collectionRef, orderBy('createdAt', 'desc'));
+
+        await onSnapshot(q, (querySnapshot) => {
+          setDocuments(
+            querySnapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            })),
+          );
+        });
+
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+
+        setLoading(false);
+      }
+    };
+
+    loadData();
+
+    return () => {
+      isCancelled.current = true;
+    };
+  }, [docCollection, search, uid]);
+
+  return { documents, error, loading };
+};
+
+export { useFetchDocuments };
