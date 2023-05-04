e7ac559160562045a5e09a637bafa10aecd817bc
abnerfirmino <abnercasteliano@outlook.com>
2023-05-03 23:56:43 -0300

    footer ajustado

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import './styles.css';
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer>
       <h3>Escreva sobre o que você tem interesse!</h3>
       <p>Mini Blog &copy; 2023</p>
     </footer>
