0be84783d7cb37e8113a1875beb56b48dad1aa09
abnerfirmino <abnercasteliano@outlook.com>
2023-05-04 00:27:07 -0300

    exibição dos detalhes dos posts criada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,11 +2,13 @@ import './styles.css';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import { PostDetails } from '../../components/PostDetails';
 
 const Home = () => {
   const { userName } = useAuthContext();
   const [query, setQuery] = useState('');
-  const [posts] = useState([]);
+  const { documents: posts, error, loading } = useFetchDocuments('posts');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -28,10 +30,22 @@ const Home = () => {
         </button>
       </form>
       <div>
-        <h1>Posts...</h1>
+        <h1>Posts</h1>
+        {loading && (
+          <p>
+            <strong>Carregando...</strong>
+          </p>
+        )}
+        {error && (
+          <p>
+            <strong>Ocorreu um erro ao carregar os dados :(</strong>
+          </p>
+        )}
+        {posts &&
+          posts.map((post) => <PostDetails key={post.id} post={post} />)}
         {posts && posts.length === 0 && (
           <div className="noposts">
-            <p>Não foram encontrados posts :(</p>
+            <p>Nenhum post foi encontrado...</p>
             <button className="post-btn">
               <Link to="/posts/create">Novo post</Link>
             </button>
