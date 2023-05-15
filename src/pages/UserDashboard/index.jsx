c260252cbfc176b7d8c9f383fefca1e4bbcab101
abnerfirmino <abnercasteliano@outlook.com>
2023-05-15 19:44:39 -0300

    dashboard do usuário criado

--- src/pages/UserDashboard/index.jsx
+++ src/pages/UserDashboard/index.jsx
@@ -1,9 +1,92 @@
 import './styles.css';
+import { Link } from 'react-router-dom';
+// hooks
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import { useAuthContext } from '../../hooks/useAuthContext';
+
+// components
+import { PostDetails } from '../../components/PostDetails';
 
 const UserDashboard = () => {
+  const { user } = useAuthContext();
+  const uid = user.uid;
+
+  // armazenamento dos posts do usuário
+  const {
+    documents: posts,
+    error,
+    loading,
+  } = useFetchDocuments('posts', null, uid);
+
+  const deleteDocument = (id) => {
+    return;
+  };
+
   return (
-    <div>
-      <h1>UserDashboard</h1>
+    <div className="dashboard">
+      <h2 className="title">Dashboard</h2>
+      <p className="desc">Gerencie seus posts!</p>
+      {loading && (
+        <p>
+          <strong>Carregando...</strong>
+        </p>
+      )}
+      {error && (
+        <p>
+          <strong>Ocorreu um erro ao carregar os dados :(</strong>
+        </p>
+      )}
+      {/* Exibe todos os posts do usuário */}
+      {posts && posts.length > 2 && (
+        <div className="grid">
+          {posts.map((post) => (
+            <div key={post.id}>
+              <PostDetails post={post} />
+              <div className="buttons">
+                <Link to={`posts/edit/${post.id}`} className="btn btn-outline">
+                  Editar
+                </Link>
+                {/* deletando documentos por id */}
+                <button
+                  className="btn"
+                  type="submit"
+                  onClick={() => deleteDocument(post.id)}
+                >
+                  Excluir
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+      {posts &&
+        posts.length <= 2 &&
+        posts.map((post) => (
+          <>
+            <PostDetails key={post.id} post={post} />
+            <div className="buttons">
+              <Link to={`posts/edit/${post.id}`} className="btn btn-outline">
+                Editar
+              </Link>
+              {/* deletando documentos por id */}
+              <button
+                className="btn"
+                type="submit"
+                onClick={() => deleteDocument(post.id)}
+              >
+                Excluir
+              </button>
+            </div>
+          </>
+        ))}
+      {posts && posts.length === 0 && (
+        <div className="noposts">
+          <p>Nenhum post foi encontrado...</p>
+          <button className="post-btn">
+            <Link to="/posts/create">Novo post</Link>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
